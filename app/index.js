4a5accc66fd952f2add2e40bdd31e64ede3893dc
jpila <j.pilapil@hotmail.com>
2017-06-20 12:06:46 -0400

    fixed merge conflicts

--- app/index.js
+++ app/index.js
@@ -1,41 +1,22 @@
+import main from './js/main.js';
+import Slider from './js/slider-ui.js';
+import App from './App';
+
 require('./main.scss');
 function submitButton(){
   console.log("clicked submit");
-  // ValidateEmail();
-  // this.flkty.next();
-
-// document.getElementById('form1').style.display="none";
-// document.getElementById('success-page').style.display="block";
-
-  // function onSubmitClicked(e){
-  //   e.preventDefault();
-  //
-  //   console.log("submitting form!");
-  //
-  //   var x = document.getElementById("form1");
-  //
-  //   var txt = "";
-  //   for (var i = 0; i < x.length; i++) {
-  //     txt = txt + x.elements[i].value + "<br>";
-  //
-  //    }
-  //    document.getElementById('demo').innerHTML = txt;
-  // }
-  // document.getElementById("submitButton").addEventListener('click', onSubmitClicked(), false);
+}
 
 let indexer = require('file-loader?name=../dist/index.html!./index.html');
 
-import main from './js/main.js';
-import Slider from './js/slider-ui.js';
-//import radio from './zInput.js';
-import App from './App';
+
 
 let app = new App();
 let slider = new Slider();
 
 $( document ).ready(function() {
 
-  let utils = window.fizzyUIUtils;
+let utils = window.fizzyUIUtils;
 
 let carousel = document.querySelector('.main-carousel');
 let flkty = new Flickity( carousel, {
@@ -50,19 +31,23 @@ previousButton1.addEventListener( 'click', function(event) {
   event.preventDefault();
   flkty.previous();
 });
+
 let previousButton2 = document.querySelector('.back2');
 previousButton2.addEventListener( 'click', function(event) {
   event.preventDefault();
   flkty.previous();
 });
 // next
+
 let nextButton = document.querySelector('.button--next');
 nextButton.addEventListener( 'click', function(event) {
   event.preventDefault();
   flkty.next();
 });
+
 let getReportButton = document.querySelector('.button--report');
 getReportButton.addEventListener( 'click', function(event) {
   event.preventDefault();
   flkty.next();
+});
 });
