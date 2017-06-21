e8164fb4624b780208031959e44c461e11cb4dd2
Victoria Trzcianowski <vtrzcianowski@gmail.com>
2017-06-21 16:58:10 -0400

    animated logo, updates storage functions

--- app/model/storage.js
+++ app/model/storage.js
@@ -7,22 +7,25 @@ export default class Storage {
         this.carousel = this.app.Carousel;
         this.createDefaultStorage();
       }
+//check if session storage is available
+//on clicking of button on bottom of each page, set session values to keys?
 
       createDefaultStorage(){
         console.log("inside storage creation");
         var elements = document.querySelectorAll("#form1 input");
         for (var i = 0, element; element = elements[i++];) {
           if (element.value === "")
-          console.log(element);
+          // console.log(element);
           this.ss.setItem(element.id, element.value);
-          element.addEventListener("change", this.updateStorageValue)
-          console.log($(element));
-          console.log(this.ss);
+          element.addEventListener("change", this.updateStorageValue(element.id, element.value))
+          // console.log($(element));
+          // console.log(this.ss);
         }
       }
 
-    static updateStorageValue(key ,value){
+      updateStorageValue(key ,value){
+        console.log("changed storage value");
         window.sessionStorage.setItem(key, value);
-    }
+      }
 
 }
