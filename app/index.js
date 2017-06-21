75bed9d5ae74d5001ff45789b5aa393445e7cb5e
jpila <j.pilapil@hotmail.com>
2017-06-21 12:32:20 -0400

    minor text fixes

--- app/index.js
+++ app/index.js
@@ -1,18 +1,18 @@
 // import main from './js/main.js';
 import Slider from './js/slider-ui.js';
 import App from './controller/App';
+//import Zinput from './js/zinput.js';
 
 require('./main.scss');
 
 let indexer = require('file-loader?name=../dist/index.html!./index.html');
 
-
 let app = new App();
 app.init();
 let slider = new Slider();
-
+//let zinput = new Zinput();
 
 $( document ).ready(function() {
   console.log("on ready");
-
 });
+
